facbe212a7786b21c3973252ec3cb211f907fb2e
aBulgakoff <a.p.bulgakoff@gmail.com>
2019-12-03 09:11:56 +0100

    Solved Explore BinSearch Template2.

---
+++ leetcode/bin_search/153_find_min_sorted_rotated.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import pytest
+
+test_data = [
+    ([3, 4, 5, 1, 2], 1),
+    ([4, 5, 6, 7, 0, 1, 2], 0),
+    ([], -1),
+    ([3, 1, 2], 1),
+]
+
+
+@pytest.mark.parametrize('input, output', test_data)
+def test_find_min(input, output):
+    assert Solution().findMin(input) == output
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+
+        def bin_search(left, right):
+            if left < right - 1:
+                pivot = (left + right) // 2
+            else:
+                return right if nums[right] < nums[left] else left
+
+            if nums[left] < nums[pivot]:
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot + 1
+                else:
+                    return bin_search(pivot, right)
+            else:
+                if nums[pivot - 1] > nums[pivot]:
+                    return pivot
+                else:
+                    return bin_search(left, pivot)
+
+        if nums[0] < nums[-1]:
+            return nums[0]  # rotation does not exist
+        else:
+            return nums[bin_search(0, len(nums) - 1)]
---
+++ leetcode/bin_search/162_find_peak.py
@@ -0,0 +1,45 @@
+from typing import List
+
+import pytest
+
+test_data = [
+    ([1, 2, 3, 1], (2,)),
+    ([1, 2, 1, 3, 5, 6, 4], (1, 5)),
+
+    ([1, 2, 3, 4, 3], (3,)),
+    ([3, 4, 3, 2, 1], (1,)),
+
+    ([1, 2, 3, 4, 3, 4], (3, 5)),
+    ([2, 3, 2, 1, 2, 3, 4, 8], (1, 7)),
+
+    ([1, 2, 1, 2], (1, 3)),
+    ([1, 2, 3, 4], (3,)),
+    ([1, 2, 3, 4, 5], (4,)),
+    ([4, 3, 2, 1], (0,)),
+    ([2, 1, 2, 1], (0, 2))
+
+]
+
+
+@pytest.mark.parametrize('nums, output', test_data)
+def test_find_peak(nums, output):
+    assert Solution().findPeakElement(nums) in output
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        def bin_search(left, right):
+            if left < right - 1:
+                pivot = (left + right) // 2
+            else:
+                return left if nums[left] > nums[right] else right
+
+            if nums[pivot - 1] < nums[pivot]:
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot  # found peak
+                else:
+                    return bin_search(pivot, right)  # peak could be by right side
+            else:
+                return bin_search(left, pivot)
+
+        return bin_search(0, len(nums) - 1)
