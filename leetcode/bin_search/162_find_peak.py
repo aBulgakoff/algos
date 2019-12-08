28192dd472c5c90f6409c7a7df5c82386d89006c
aBulgakoff <a.p.bulgakoff@gmail.com>
2019-12-08 08:34:32 +0100

    Solved Explore BinSearch Template3 p3.

--- leetcode/bin_search/162_find_peak.py
+++ leetcode/bin_search/162_find_peak.py
@@ -16,7 +16,8 @@ test_data = [
     ([1, 2, 3, 4], (3,)),
     ([1, 2, 3, 4, 5], (4,)),
     ([4, 3, 2, 1], (0,)),
-    ([2, 1, 2, 1], (0, 2))
+    ([2, 1, 2, 1], (0, 2)),
+    ([1, 2], (1,)),
 
 ]
 
@@ -24,6 +25,11 @@ test_data = [
 @pytest.mark.parametrize('nums, output', test_data)
 def test_find_peak(nums, output):
     assert Solution().findPeakElement(nums) in output
+
+
+@pytest.mark.parametrize('nums, output', test_data)
+def test_find_peak_sol_two(nums, output):
+    assert SolutionTwo().findPeakElement(nums) in output
 
 
 class Solution:
@@ -39,6 +45,32 @@ class Solution:
                     return pivot  # found peak
                 else:
                     return bin_search(pivot, right)  # peak could be by right side
+            else:
+                return bin_search(left, pivot)
+
+        return bin_search(0, len(nums) - 1)
+
+
+class SolutionTwo:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+        if len(nums) == 1:
+            return 0
+        if len(nums) == 2:
+            return 0 if nums[0] > nums[1] else 1
+
+        def bin_search(left, right):
+            if left < right:
+                pivot = (left + right) // 2
+            else:
+                return right
+
+            if nums[pivot] > nums[pivot - 1]:
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot
+                else:
+                    return bin_search(pivot + 1, right)
             else:
                 return bin_search(left, pivot)
 
