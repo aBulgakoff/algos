4f0c246051a429153ce386f0cf4fe0492f508c8a
aBulgakoff <a.p.bulgakoff@gmail.com>
2020-02-05 07:34:10 +0100

    Solved 3 2d array tasks

---
+++ leetcode/array/118_pascal_triangle.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for row_num in range(numRows):
+            row = [1 for _ in range(row_num+1)]
+            for col in range(1, len(row)-1):
+                row[col] = triangle[row_num-1][col-1] + triangle[row_num-1][col]
+            triangle.append(row)
+        return triangle
---
+++ leetcode/array/54_spiral_matrix.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        while matrix:
+            result.extend(matrix.pop(0))
+            matrix = [*zip(*matrix)][::-1]
+        return result
