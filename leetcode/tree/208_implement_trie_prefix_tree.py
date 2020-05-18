6e2968bf4d8fdbdca6a5eeee982ce95ca1da20ab
aBulgakoff <a.p.bulgakoff@gmail.com>
2020-05-18 11:22:21 +0200

    Solved 1 task.

--- leetcode/tree/208_implement_trie_prefix_tree.py
+++ leetcode/tree/208_implement_trie_prefix_tree.py
@@ -44,9 +44,58 @@ class Trie:
             return False
         return True
 
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+from collections import defaultdict
+from functools import reduce
 
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = lambda: defaultdict(self.trie)
+        self.root = self.trie()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+
+        reduce(dict.__getitem__, word, self.root)['.'] = word
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for ch in word:
+            if ch in node:
+                node = node[ch]
+            else:
+                return False
+        return node.get('.') == word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for ch in prefix:
+            if ch in node:
+                node = node[ch]
+            else:
+                return False
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
