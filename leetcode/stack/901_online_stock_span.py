e0b396977e24c389670c8733d71a4378a1d5cfcd
aBulgakoff <a.p.bulgakoff@gmail.com>
2020-05-20 23:34:52 +0200

    Solved 2 tasks.

---
+++ leetcode/stack/901_online_stock_span.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][1] <= price:
+            span += self.stack.pop()[0]
+        self.stack.append((span, price))
+        return span
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
